038220267eff11d244e90fe5cb939c27f1b41e4a
Lunarch <phantanlocct@outlook.com>
2026-09-01 09:55:28 +0700

    feat: use settings tab with icon browser

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { Notice, Plugin } from "obsidian";
 import { DockController } from "./dock";
-import { LedgeSettingTab } from "./settings-tab";
+import { LedgeSettingTabWithIcons } from "./settings-tab-icons";
 import { hasLegacyHotCornerSettings, normalizeSettings } from "./settings";
 import type { LedgeSettings } from "./types";
 
@@ -14,7 +14,7 @@ export default class LedgePlugin extends Plugin {
     const shouldPersistTriggerMigration = hasLegacyHotCornerSettings(storedSettings);
     this.settings = normalizeSettings(storedSettings);
     if (shouldPersistTriggerMigration) await this.saveData(this.settings);
-    this.addSettingTab(new LedgeSettingTab(this.app, this));
+    this.addSettingTab(new LedgeSettingTabWithIcons(this.app, this));
 
     this.addCommand({
       id: "toggle-dock",
